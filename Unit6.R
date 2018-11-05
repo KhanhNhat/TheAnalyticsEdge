2d12dfd901149082580d4848cb4fd37d4f14256f
KhanhNhat <hoangphamkhanhnhat@gmail.com>
2018-11-05 22:45:30 +1100

    Unit 6: Complete Asignment 1

--- Unit6.R
+++ Unit6.R
@@ -43,3 +43,79 @@ df1 <- data.frame(rows = c("one","two","three"),two = 1:3,three=1:3)
 df1 %>%
   gather(newrows,valname,-rows) %>%
   spread(rows,valname)
+
+#Unit 6: Assignment 1
+dailykos = read_csv('dailykos.csv')
+
+word_dist = dist(dailykos, method = 'euclidean')
+word_hc = hclust(word_dist, method = 'ward.D')
+
+library(dendextend)
+
+cluster7 = cutree(word_hc, k = 7)
+
+#How many article in each cluster
+dailykos %>% mutate(cluster = cluster7) %>%
+             group_by(cluster) %>%
+             summarise(ArticleInCluster = n())
+
+#Find the 6 most common word in cluster 1
+word_cl1 = dailykos %>% mutate(cluster = cluster7) %>%
+                        filter(cluster == 1) %>%
+                        summarise_all(.funs = sum) %>% t(.)
+
+word_cl1_df = data.frame(word = row.names(word_cl1), numAppears = word_cl1)
+word_cl1_df %>% arrange(desc(numAppears)) %>% head(.)
+
+#How about cluster 2?
+word_cl2 = dailykos %>% mutate(cluster = cluster7) %>%
+                        filter(cluster == 2) %>%
+                        summarise_all(.funs = sum) %>% t(.)
+
+word_cl2_df = data.frame(word = row.names(word_cl2), numAppears = word_cl2)
+word_cl2_df %>% arrange(desc(numAppears)) %>% head(.)
+
+#How about other?
+clusterSummary = dailykos %>% mutate(cluster = cluster7) %>%
+                              group_by(cluster) %>%
+                              summarise_all(.funs = sum) %>% t(.) %>% as.data.frame(.)
+
+clusterSummary$word = row.names(clusterSummary)
+
+clusterSummary %>% filter(word == 'iraq')
+clusterSummary %>% filter(word %in% c('john', 'dean', 'vice', 'kerry', 'howard', 'bush'))
+
+#Now try with kmeans()
+set.seed(1000)
+word_km = kmeans(dailykos, centers = 7)
+
+dailykos %>% mutate(cluster = word_km$cluster) %>%
+             group_by(cluster) %>%
+             summarise(NumObservations = n())
+
+kmSummary = dailykos %>% mutate(cluster = word_km$cluster) %>%
+                         group_by(cluster) %>%
+                         summarise_all(.funs = sum) %>% t(.) %>% as.data.frame(.)
+
+kmSummary$word = row.names(kmSummary)
+
+#Which cluster talks about 'iraq'
+kmSummary %>% filter(word == 'iraq')
+
+#Which cluster talks about Democratic party
+kmSummary %>% filter(word %in% c('john', 'dean', 'vice', 'kerry', 'howard', 'bush'))
+
+#Which Hierarchical Cluster best corresponds to K-Means Cluster 2, 3, 7, 6?
+clusterSummary %>% map_dbl(~sum(.x == kmSummary$V2))
+
+clusterSummary %>% map_dbl(~sum(.x == kmSummary$V3))
+
+clusterSummary %>% map_dbl(~sum(.x == kmSummary$V7))
+
+clusterSummary %>% map_dbl(~sum(.x == kmSummary$V6))
+
+
+
+
+
+
